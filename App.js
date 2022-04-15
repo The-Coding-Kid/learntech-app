5ca6e1ac293c1f7ce0fa1441d68fdce7844a1da5
The-Coding-Kid <veerrohitv08@gmail.com>
2022-04-15 16:58:57 -0700

    added some dummy data

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import MapView from "react-native-maps";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map} showsUserLocation={true} initialRegion={{
+        latitude: 37.7812370,
+        longitude: -122.4108242,
+        latitudeDelta: 0.1,
+        longitudeDelta: 0.05,
+      }}>
+      </MapView>
     </View>
   );
 }
@@ -13,8 +20,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
 });
---
+++ data.js
@@ -0,0 +1,14 @@
+let data = [
+    {
+        "id": 1,
+        "name": "TeamSeas",
+        "location": {
+            "city": "San Francisco",
+            "address": "420 Hayes St, San Francisco, CA 94102",
+            "latitude": 37.7749295,
+            "longitude": -122.4194155
+        },
+        "phone": "(415) 564-8500",
+    },
+
+]
